99ba78e08a5dc90fddf5dd50a35841e6952a269b
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-16 17:48:16 +0200

    Add router, finish up welcome

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Tab {
+  icon: string;
+  content: string;
+  title: string;
+}
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+
+  tabs: Tab[] = [
+    { title: 'Homeland', icon: 'home', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem, quod expedita impedit doloribus recusandae dignissimos assumenda delectus. Omnis, odit.'},
+    { title: 'Career', icon: 'home', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem, quod expedita impedit doloribus recusandae dignissimos assumenda delectus. Omnis, odit.'},
+    { title: 'Religion', icon: 'home', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem, quod expedita impedit doloribus recusandae dignissimos assumenda delectus. Omnis, odit.'},
+  ]
+
+  welcomeMsg: string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem, quod expedita impedit doloribus recusandae dignissimos assumenda delectus. Omnis, odit.';
 
   constructor() { }
 
