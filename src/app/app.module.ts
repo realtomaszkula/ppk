38b05bb077ad0ee6a57383a021b2da12355c6b3d
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-16 08:58:52 +0200

    Add tabs for welcome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { SliderComponent } from './slider/slider.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SliderComponent
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -1,11 +1,11 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
-import { SliderComponent } from './slider.component';
+import { WelcomeComponent } from './welcome.component';
 
-describe('Component: Slider', () => {
+describe('Component: Welcome', () => {
   it('should create an instance', () => {
-    let component = new SliderComponent();
+    let component = new WelcomeComponent();
     expect(component).toBeTruthy();
   });
 });
