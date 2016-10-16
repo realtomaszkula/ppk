16a23d91db5d927a4aaac113cc3b426ad3fb9ec8
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-16 08:20:03 +0200

    Add animation for nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, animate, style, transition } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({ height: '*' })),
+      state('close', style({ height: 0 })),
+      transition('close <=> open', animate(200))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  open = false;
 }
