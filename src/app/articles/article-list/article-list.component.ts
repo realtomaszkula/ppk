bae7f80337aff8956ae96126aa2a7274bb33f669
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-19 01:34:39 +0200

    Add navigation to articles

--- src/app/articles/article-list/article-list.component.ts
+++ src/app/articles/article-list/article-list.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+interface ArticleSummary {
+  id: number;
+  name: string,
+  icon: string,
+  excerpt: string
+}
+
+
 
 @Component({
   selector: 'app-article-list',
@@ -7,9 +17,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleListComponent implements OnInit {
 
-  constructor() { }
+  articlesSummaries: ArticleSummary[] = [
+    { id: 1,  name: 'Stres', excerpt: `Amet tempor do laborum ex deserunt et proident minim quis nulla eu. Labore incididunt elit labore officia duis pariatur nostrud ea irure ullamco veniam. Ea sit quis id aute. Deserunt proident eiusmod aliquip fugiat voluptate commodo veniam eu. Do ipsum incididunt officia ut non proident non qui.`, icon: 'home'},
+    { id: 2,  name: 'Stres', excerpt: `Qui qui mollit consectetur culpa. Id aliquip ullamco ea incididunt aliqua. Ea non est velit aliquip sunt. Officia quis duis laboris qui non eiusmod ullamco irure. Excepteur cupidatat fugiat et nisi enim enim veniam id reprehenderit occaecat velit duis sint. Fugiat eiusmod nisi pariatur ipsum ut ea.`, icon: 'pregnant_woman'},
+    { id: 3,  name: 'Stres', excerpt: `Cillum eu aliqua ullamco sint laboris eiusmod eu.`, icon: 'query_builder'},
+    { id: 4,  name: 'Stres', excerpt: `Commodo qui eu ea aliqua mollit consequat qui cupidatat anim laborum fugiat fugiat. Veniam occaecat ullamco voluptate qui minim non dolore ex. Amet velit aliquip aliqua Lorem duis cupidatat.`, icon: 'motorcycle'},
+    { id: 5,  name: 'Stres', excerpt: `Commodo reprehenderit reprehenderit ad reprehenderit labore et velit consectetur minim ea.`, icon: 'opacity'},
+  ]
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+  }
+
+  navigateTo(id: number) {
+    this.router.navigate([id], { relativeTo: this.route });
   }
 
 }
--- src/app/articles/articles.routing.ts
+++ src/app/articles/articles.routing.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
 
 const routes: Routes = [
-  { path: '', component: ArticleListComponent }
+  { path: '', component: ArticleListComponent },
+  { path: ':id', component: ArticleDetailComponent }
 ]
 
 export const articlesRouting: ModuleWithProviders = RouterModule.forChild(routes);
