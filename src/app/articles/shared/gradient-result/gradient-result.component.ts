3b3e5e25769c30197c6367e8e837a550ab508c6c
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-20 00:32:22 +0200

    Add gradient-result

--- src/app/articles/shared/gradient-result/gradient-result.component.ts
+++ src/app/articles/shared/gradient-result/gradient-result.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gradient-result',
   template: `
+  <div *ngIf="result >= 0">
+
+    <h2>Result</h2>
+
+    <h4>You scored: {{ result }}</h4>
+    <p>{{ resultExplanation }} </p>
     <section>
-        Test
+      <div class="line" [style.left]="left"></div>
     </section>
+  </div>
   `,
   styleUrls: ['./gradient-result.component.scss']
 })
-export class GradientResultComponent implements OnInit {
+export class GradientResultComponent {
 
-  constructor() { }
+  @Input() result: number = 0;
 
-  ngOnInit() {
+  resultExplanation: string = 'Cillum nisi Lorem occaecat enim enim dolore velit do adipisicing ut. Nulla ea deserunt do reprehenderit nisi adipisicing in ullamco id eiusmod adipisicing amet. Reprehenderit sunt ex laboris consequat commodo pariatur aliqua id excepteur sint. Deserunt irure qui minim enim fugiat laboris labore. Mollit magna sunt in consequat nostrud magna eiusmod aliqua. Anim ex nulla laborum irure dolore sunt nostrud ut. Ipsum sint exercitation ullamco velit.';
+
+  get left() {
+    return this.result + '%'
   }
 
 }
