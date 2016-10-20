c81d2600a05713a2dd77d55567df4f9a5b243779
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-20 07:54:04 +0200

    Move logic inside line-break

--- src/app/articles/shared/line-break/line-break.component.ts
+++ src/app/articles/shared/line-break/line-break.component.ts
@@ -1,11 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-line-break',
   templateUrl: './line-break.component.html',
-  styleUrls: ['./line-break.component.scss']
+  styleUrls: ['./line-break.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LineBreakComponent  {
-  @Input() show: boolean;
-  @Input() text: string;
+
+  private modulo: number;
+  private _length: number;
+
+  @Input() index: number;
+  @Input() set length(value: number) {
+    this.modulo = value - 1 % 10;
+    this._length = value - 1;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get show(): boolean {
+    return (this.index - this.modulo) % 10 === 0 && this.length > 20 && this.index > 10 && this.index !== this.length;
+  }
+
+  get text(): string {
+    return `${this.length  - this.index} questions left`;
+  }
 }
+
+
+
