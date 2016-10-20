3b3de820eb854dd392e5a0abd8c8fb5707bd128b
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-20 06:30:44 +0200

    mend

--- src/app/articles/sensation-seeking/sensation-seeking.component.ts
+++ src/app/articles/sensation-seeking/sensation-seeking.component.ts
@@ -20,50 +20,50 @@ export class SensationSeekingComponent implements OnInit {
       new Question('W rześki, chłodny dzień czuję się orzeżwiony i pełen animuszu.',	1),
       new Question('Kiedy na dworze jest zmino marze o tym, aby wejść do środka.',	0)
     ],
-    // [
-    //   new Question('Jestem znudzony oglądanie tych samych, znanych od dawna rzeczy.',	1),
-    //   new Question('Lubię komfort przebywania z tymi samymi od lat, dobrze znanymi ludźmi.',	0)
-    // ],
-    // [
-    //   new Question('Wolałbym żyć w społeczeństwie idealnym, w którym każdy czuje się bezpieczny, pewny i szczęśliwy.',	0),
-    //   new Question('Zdecydowanie wole żyć w dzisiejszych niespokojnych czasach.',	1)
-    // ],
-    // [
-    //   new Question('Lubię czasami robić rzeczy, które są nieco przerażające.',	1),
-    //   new Question('Człowiek rozsądny unika czynności lub zachowań, które są niebezpieczne.',	0)
-    // ],
-    // [
-    //   new Question('Nie chciałbym poddać się hipnozie.',	0),
-    //   new Question('Chciałbym się przekonać, jak to jest zostać zahipnotyzowanym.',	1)
-    // ],
-    // [
-    //   new Question('Najważniejszym celem w życiu jest życ pełnią życia i doświadczać wszystkiego, co tylko możliwe.',	1),
-    //   new Question('Najważniejszy celem w życiu jest odnalezienie spokoju i szczęścia.',	0)
-    // ],
-    // [
-    //   new Question('Chciałbym spróbowac skoków na spadochronie.',	1),
-    //   new Question('Za nic w świecie nie chciałbym próbowac skakać z samolotu, ani na spadochronie, ani bez.',	0)
-    // ],
-    // [
-    //   new Question('Do zimnej wody wchodzę stopniowo, dając sobie czas na oswojenie się z niską temperaturą.',	0),
-    //   new Question('Lubię nurkować lub skakać prosto do oceanu lub chłodnego basenu.',	1)
-    // ],
-    // [
-    //   new Question('Kiedy jadę na urlop, preferuję dobry pokój i wygodne łóżko.',	0),
-    //   new Question('Kiedy jadę na urlop, wybieram namiot i biwakowanie na świeżym powietrzu.',	1)
-    // ],
-    // [
-    //   new Question('Wolę ludzi ekspresyjnych, otwarcie wyrażających swoje emocje, nawet jeśli bywają nieco niezrównoważeni.',	1),
-    //   new Question('Wolę ludzi spokojnych i opanowanych, nieulegających nastrojom.',	0)
-    // ],
-    // [
-    //   new Question('Dobry obraz powinien szokowac lub wstrząsać, pobudzając zmysly.',	1),
-    //   new Question('Dobry obraz powinien wywoływać poczucie spokoju i bezpieczeństwa.',	0)
-    // ],
-    // [
-    //   new Question('Motocykliści muszą mieć jakąś nieuświadomioną potrzebę narażania się na niebezpieczeństwo.',	1),
-    //   new Question('Chciałbym przejechac się motocyklem i umieć go prowadzić.',	)
-    // ],  
+    [
+      new Question('Jestem znudzony oglądanie tych samych, znanych od dawna rzeczy.',	1),
+      new Question('Lubię komfort przebywania z tymi samymi od lat, dobrze znanymi ludźmi.',	0)
+    ],
+    [
+      new Question('Wolałbym żyć w społeczeństwie idealnym, w którym każdy czuje się bezpieczny, pewny i szczęśliwy.',	0),
+      new Question('Zdecydowanie wole żyć w dzisiejszych niespokojnych czasach.',	1)
+    ],
+    [
+      new Question('Lubię czasami robić rzeczy, które są nieco przerażające.',	1),
+      new Question('Człowiek rozsądny unika czynności lub zachowań, które są niebezpieczne.',	0)
+    ],
+    [
+      new Question('Nie chciałbym poddać się hipnozie.',	0),
+      new Question('Chciałbym się przekonać, jak to jest zostać zahipnotyzowanym.',	1)
+    ],
+    [
+      new Question('Najważniejszym celem w życiu jest życ pełnią życia i doświadczać wszystkiego, co tylko możliwe.',	1),
+      new Question('Najważniejszy celem w życiu jest odnalezienie spokoju i szczęścia.',	0)
+    ],
+    [
+      new Question('Chciałbym spróbowac skoków na spadochronie.',	1),
+      new Question('Za nic w świecie nie chciałbym próbowac skakać z samolotu, ani na spadochronie, ani bez.',	0)
+    ],
+    [
+      new Question('Do zimnej wody wchodzę stopniowo, dając sobie czas na oswojenie się z niską temperaturą.',	0),
+      new Question('Lubię nurkować lub skakać prosto do oceanu lub chłodnego basenu.',	1)
+    ],
+    [
+      new Question('Kiedy jadę na urlop, preferuję dobry pokój i wygodne łóżko.',	0),
+      new Question('Kiedy jadę na urlop, wybieram namiot i biwakowanie na świeżym powietrzu.',	1)
+    ],
+    [
+      new Question('Wolę ludzi ekspresyjnych, otwarcie wyrażających swoje emocje, nawet jeśli bywają nieco niezrównoważeni.',	1),
+      new Question('Wolę ludzi spokojnych i opanowanych, nieulegających nastrojom.',	0)
+    ],
+    [
+      new Question('Dobry obraz powinien szokowac lub wstrząsać, pobudzając zmysly.',	1),
+      new Question('Dobry obraz powinien wywoływać poczucie spokoju i bezpieczeństwa.',	0)
+    ],
+    [
+      new Question('Motocykliści muszą mieć jakąś nieuświadomioną potrzebę narażania się na niebezpieczeństwo.',	1),
+      new Question('Chciałbym przejechac się motocyklem i umieć go prowadzić.',	)
+    ],  
     ]
 
 
