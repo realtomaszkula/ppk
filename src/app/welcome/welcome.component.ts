1c0128f519bcbf705b4158150d7e8accc33dd084
Tomasz Kula <tomaszkula07@gmail.com>
2016-10-20 21:41:59 +0200

    Hook up icon links

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -4,6 +4,7 @@ interface Tab {
   icon: string;
   content: string;
   title: string;
+  url: string;
 }
 
 @Component({
@@ -14,9 +15,9 @@ interface Tab {
 export class WelcomeComponent implements OnInit {
 
   tabs: Tab[] = [
-    { title: 'Homeland', icon: 'home', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem, quod expedita impedit doloribus recusandae dignissimos assumenda delectus. Omnis, odit.'},
-    { title: 'Career', icon: 'home', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem, quod expedita impedit doloribus recusandae dignissimos assumenda delectus. Omnis, odit.'},
-    { title: 'Religion', icon: 'home', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem, quod expedita impedit doloribus recusandae dignissimos assumenda delectus. Omnis, odit.'},
+    { url: '/contact', title: 'Contact', icon: 'home', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem, quod expedita impedit doloribus recusandae dignissimos assumenda delectus. Omnis, odit.'},
+    { url: '/career', title: 'Career', icon: 'home', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem, quod expedita impedit doloribus recusandae dignissimos assumenda delectus. Omnis, odit.'},
+    { url: '/articles', title: 'Articles', icon: 'home', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem, quod expedita impedit doloribus recusandae dignissimos assumenda delectus. Omnis, odit.'},
   ]
 
   welcomeMsg: string = 'Id fugiat ex occaecat cupidatat sit pariatur dolore aute in mollit. Cupidatat labore Lorem pariatur amet enim et id laborum officia. Cupidatat do laboris reprehenderit ea reprehenderit voluptate esse anim ullamco. Mollit fugiat duis consequat sit cillum ipsum quis anim sunt adipisicing tempor duis elit tempor. Eiusmod deserunt quis non tempor. Ex nisi proident officia dolor consequat cupidatat. Ipsum cillum ut mollit do ex duis.';
